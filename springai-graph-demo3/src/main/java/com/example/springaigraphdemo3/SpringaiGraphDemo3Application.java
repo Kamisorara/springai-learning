b0f88ae22479297d2dd5488a4262d75e3ed116ec
waylon <rakamiso575@gmail.com>
2025-11-25 09:02:02 +0800

    springai-graph-demo3 init

---
+++ springai-graph-demo3/src/main/java/com/example/springaigraphdemo3/SpringaiGraphDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.springaigraphdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiGraphDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiGraphDemo3Application.class, args);
+    }
+
+}
---
+++ springai-graph-demo3/src/test/java/com/example/springaigraphdemo3/SpringaiGraphDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaigraphdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiGraphDemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
