382e278d5cec462edbe9b9462ced2fd504a9e8dd
Kamisora <rakamiso575@gmail.com>
2025-11-30 13:26:13 +0800

    OpenAiModelCompatibleConfig reflector

--- springai-graph-demo3/src/main/java/com/example/springaigraphdemo3/config/OpenAiModelCompatibleConfig.java
+++ springai-graph-demo3/src/main/java/com/example/springaigraphdemo3/config/OpenAiModelCompatibleConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
  * 用于修改 OpenAI 模型的请求 URL，将 /v1/chat/completions 替换为 /chat/completions 增强切换模型API的兼容性
  */
 @Configuration
-public class OpenAiModelOpenAiConfig {
+public class OpenAiModelCompatibleConfig {
 
     @Value("${spring.ai.openai.api-key}")
     private String apiKey;
