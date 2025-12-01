7b0fdb69aa37c84eab345d8f4e17dd82ac3084ec
waylon <rakamiso575@gmail.com>
2025-12-01 08:52:23 +0800

    init springai-rag-demo1

---
+++ springai-rag-demo1/src/main/java/com/example/springairagdemo1/SpringaiRagDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.springairagdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiRagDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiRagDemo1Application.class, args);
+    }
+
+}
---
+++ springai-rag-demo1/src/test/java/com/example/springairagdemo1/SpringaiRagDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springairagdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiRagDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
